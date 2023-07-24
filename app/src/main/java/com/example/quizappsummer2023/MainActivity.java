98966e5aab2f353168a4b9e9bc7f97d408bb756e
Tanika Johnson <tanika.johnson@pgcps.org>
2023-07-24 17:36:00 -0400

    added code to display the score. added a string to track the score but didn't code the string.

--- app/src/main/java/com/example/quizappsummer2023/MainActivity.java
+++ app/src/main/java/com/example/quizappsummer2023/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
 
     TextView questionTV;
     Button falseButton, trueButton;
+    int rightAnswer;
+    int wrongAnswer;
+    String[] Answer = ("rightAnswer","wrongAnswer");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +43,21 @@ public class MainActivity extends AppCompatActivity {
                 Toast toast = Toast.makeText(getApplicationContext(), text, duration);
                 toast.show();
             }
+            if (rightAnswer = 1){
+                CharSequence text = "Right Answer = 1 and Wrong Answer = 0";
+                int duration = Toast.LENGTH_SHORT;
 
-        });
+                Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+                toast.show();
+            }else {
+                CharSequence text = "Right Answer =0 and Wrong Answer = 1";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+                toast.show();
+            }
+
+        })
+
     }
 }
