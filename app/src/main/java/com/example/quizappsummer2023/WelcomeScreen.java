6394f400e5e2fc62a32e54161c770e7d1a948435
johnsontanika <tanika.johnson@pcps.org>
2023-07-26 10:44:20 -0400

    changed the mainfest to have the welcomeScreen to be the launcher.

--- app/src/main/java/com/example/quizappsummer2023/WelcomeScreen.java
+++ app/src/main/java/com/example/quizappsummer2023/WelcomeScreen.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class WelcomeScreen extends AppCompatActivity {
 
     TextView welcomeTV;
-
+    Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +19,15 @@ public class WelcomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_screen);
 
         welcomeTV = (TextView) findViewById(R.id.welcomeTV);
+        nextButton = (Button) findViewById(R.id.nextButton);
 
         Intent incomingIntent = getIntent();
 
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
